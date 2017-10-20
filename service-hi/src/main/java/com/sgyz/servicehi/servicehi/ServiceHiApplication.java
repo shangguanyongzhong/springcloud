9531303b6caeac866ee281df03e6767c3090fad8
上官永忠 <wb-sgyz272053@alibaba-inc.com>
2017-10-20 17:44:52 +0800

    spring cloud server build ok！

---
+++ service-hi/src/main/java/com/sgyz/servicehi/servicehi/ServiceHiApplication.java
@@ -0,0 +1,12 @@
+package com.sgyz.servicehi.servicehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+}
