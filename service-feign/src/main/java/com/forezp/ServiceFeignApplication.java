d8d769767ed8c61aefd82d8a69cfcfc6174cb209
上官永忠 <wb-sgyz272053@alibaba-inc.com>
2017-12-11 11:17:29 +0800

    服务消费者（Feign）

---
+++ service-feign/src/main/java/com/forezp/ServiceFeignApplication.java
@@ -0,0 +1,19 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * Created by sgyz(上官永忠) on 2017/12/5.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ServiceFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFeignApplication.class, args);
+    }
+}
--- serviceribbon/src/main/java/com/forezp/service/HelloService.java
+++ serviceribbon/src/main/java/com/forezp/service/HelloService.java
@@ -11,8 +11,12 @@ import org.springframework.web.client.RestTemplate;
 public class HelloService {
     @Autowired
     RestTemplate restTemplate;
-
+    //@HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
